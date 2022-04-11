7e468ae69ff7879a1ac014cd001eb96f686c1dd6
Idan Izicovich <idanizi@gmail.com>
2022-04-11 17:43:50 +0300

    wip

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
 #!/usr/bin/env node
 
-const { promisify } = require('util')
+const {promisify} = require('util')
 const exec = promisify(require('child_process').exec)
 const prompts = require('prompts')
 
-async function run () {
-  const { stdout: branches } = await exec('git branch -v --sort=-committerdate')
+async function run() {
+    const {stdout: branches} = await exec('git branch -v --sort=-committerdate')
 
-  const choices = branches
-    .split(/\n/)
-    .filter(branch => !!branch.trim())
-    .map(branch => {
-      const [, flag, value, hint] = branch.match(/([* ]) +([^ ]+) +(.+)/)
-      return { value, hint, disabled: flag === '*' }
+    const choices = branches
+        .split(/\n/)
+        .filter(branch => !!branch.trim())
+        .map(branch => {
+            const [, flag, value, hint] = branch.match(/([* ]) +([^ ]+) +(.+)/)
+            return {value, hint, disabled: flag === '*'}
+        })
+
+    const {selectedBranches} = await prompts({
+        type: 'multiselect',
+        name: 'selectedBranches',
+        message: 'Delete branches',
+        choices,
+        hint: choices[0].hint,
+        warn: 'current branch',
+        // onState({value}) {
+        //     console.log({'this.hint': this.hint, choices})
+        //     // this.hint = choices.find(c => c.value === value).hint
+        // }
     })
 
-  const { branch } = await prompts({
-    type: 'select',
-    name: 'branch',
-    message: 'Switch branch',
-    choices,
-    hint: choices[0].hint,
-    warn: 'current branch',
-    onState ({ value }) {
-      this.hint = choices.find(c => c.value === value).hint
+    await deleteBranches(selectedBranches)
+}
+
+async function deleteBranches(branches) {
+    console.log({branches})
+    // if (!branches) return
+    // for (let branch of branches) {
+    //     const {stdout, stderr} = await exec(`git branch -d ${branch}`)
+    //     process.stdout.write(stdout)
+    //     process.stderr.write(stderr)
+    // }
+}
+
+function onError(e) {
+    if (e.stderr) {
+        process.stderr.write(e.stderr)
+    } else {
+        console.error(e)
     }
-  })
-
-  await checkout(branch)
-}
-
-async function checkout (branch) {
-  if (!branch) return
-  const { stdout, stderr } = await exec(`git checkout ${branch}`)
-  process.stdout.write(stdout)
-  process.stderr.write(stderr)
-}
-
-function onError (e) {
-  if (e.stderr) {
-    process.stderr.write(e.stderr)
-  } else {
-    console.error(e)
-  }
 }
 
 run().catch(onError)
