5b6c548ba573fd38943d1ddb725d6be1380d830d
Idan Izicovich <idanizi@gmail.com>
2022-04-12 00:54:32 +0300

    try catch errors

--- index.js
+++ index.js
@@ -5,38 +5,46 @@ const exec = promisify(require('child_process').exec)
 const prompts = require('prompts')
 
 async function run() {
-    const {stdout: branches} = await exec('git branch -v --sort=-committerdate')
+    try {
+        const {stdout: branches} = await exec('git branch -v --sort=-committerdate')
 
-    const choices = branches
-        .split(/\n/)
-        .filter(branch => !!branch.trim())
-        .map(branch => {
-            const [, flag, value, hint] = branch.match(/([* ]) +([^ ]+) +(.+)/)
-            return {value, hint, disabled: flag === '*'}
+        const choices = branches
+            .split(/\n/)
+            .filter(branch => !!branch.trim())
+            .map(branch => {
+                const [, flag, value, hint] = branch.match(/([* ]) +([^ ]+) +(.+)/)
+                return {value, hint, disabled: flag === '*'}
+            })
+
+        const {selectedBranches} = await prompts({
+            type: 'multiselect',
+            name: 'selectedBranches',
+            message: 'Delete branches (right arrow key to select, `a` to select all)',
+            choices,
+            hint: choices[0].hint,
+            warn: 'current branch',
+            instructions: false,
+            onRender() {
+                this.hint = choices[this.cursor].hint
+            }
         })
 
-    const {selectedBranches} = await prompts({
-        type: 'multiselect',
-        name: 'selectedBranches',
-        message: 'Delete branches (right arrow key to select, `a` to select all)',
-        choices,
-        hint: choices[0].hint,
-        warn: 'current branch',
-        instructions: false,
-        onRender() {
-            this.hint = choices[this.cursor].hint
-        }
-    })
-
-    await deleteBranches(selectedBranches)
+        await deleteBranches(selectedBranches)
+    } catch (err) {
+        onError(err)
+    }
 }
 
 async function deleteBranches(branches) {
     if (!branches) return
     for (let branch of branches) {
-        const {stdout, stderr} = await exec(`git branch -d ${branch}`)
-        process.stdout.write(stdout)
-        process.stderr.write(stderr)
+        try {
+            const {stdout, stderr} = await exec(`git branch -d ${branch}`)
+            process.stdout.write(stdout)
+            process.stderr.write(stderr)
+        } catch (err) {
+            onError(err)
+        }
     }
 }
 
